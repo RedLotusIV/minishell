3c4373ba11c45ea173bcb97de57befb3ba3c5493
Reda Moutaouakkil <rmoutaou@e2r3p10.1337.ma>
2024-05-16 23:39:04 +0100

    file structure upated

---
+++ include/minishell.h
@@ -0,0 +1,20 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <signal.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <limits.h>
+typedef struct s_TreeNode
+{
+	void				*data;
+	struct s_TreeNode	*left;
+	struct s_TreeNode	*right;
+}				t_TreeNode;
+
+#endif
