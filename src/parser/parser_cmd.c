a81f0a73913fe13095fc73371bffc54632051073
Adnane Mouhandiz <109529929+RedLotusIV@users.noreply.github.com>
2024-06-15 20:49:54 +0000

    nothing

--- src/parser/parser_cmd.c
+++ src/parser/parser_cmd.c
@@ -12,14 +12,6 @@
 
 #include "../../include/parser.h"
 
-// void	allocate_cmd(t_cmd *cmd, t_token *token)
-// {
-	// i should count how many commands i have in the tokens, meaning the number of pipes + 1
-	// i should allocate the number of commands i have
-	// i should then allocate the number of arguments i have in each command as a char **
-	// so in the end i should have an array of commands, each command has a char ** of arguments and a t_token of redirections
-	// which will make it so easy to be executed in the execution part, with pipes and redirections and everything
-// }ex
 t_cmd	**parse_cmd(t_token *head)
 {
 	t_cmd	**cmd;
